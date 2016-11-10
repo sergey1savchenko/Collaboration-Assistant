b9f321e5a5f45aede393136def8cf18c4655490e
Vera <chuiko.vera@gmail.com>
2016-11-10 17:37:40 +0200

    Team model

--- src/main/java/loki/ca/model/Team.java
+++ src/main/java/loki/ca/model/Team.java
@@ -1,12 +1,20 @@
 package loki.ca.model;
 
-
-/**
- * Temporary Team entity stub
- */
 public class Team {
 
 	private int id;
+	private int project_id;
+	private String title;
+	
+	public Team(){
+		
+	}
+	
+	public Team(int id, int pr_id, String title){
+		this.id = id;
+		this.project_id = pr_id;
+		this.title = title;
+	}
 
 	public int getId() {
 		return id;
@@ -14,6 +22,28 @@ public class Team {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public int getProject_id() {
+		return project_id;
+	}
+
+	public void setProject_id(int project_id) {
+		this.project_id = project_id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+	@Override
+	public String toString(){
+		return "Team [id = " + this.id + ", project_id = " + this.project_id + ", title = " + title + "]"; 
 	}
 	
 }
