ec45d8b4a59f061c6b52b7371fe95da518856e50
Sergey <w1nner1715@gmail.com>
2016-11-27 21:53:44 +0200

    add TeamController

---
+++ src/main/java/com/netcracker/ca/controller/api/TeamController.java
@@ -0,0 +1,36 @@
+package com.netcracker.ca.controller.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.netcracker.ca.model.Team;
+import com.netcracker.ca.service.TeamService;
+
+import java.util.List;
+
+@RestController
+public class TeamController {
+
+    @Autowired
+    private TeamService teamService;
+
+    @RequestMapping(value = "/team", method = RequestMethod.GET, produces = "application/json")
+    public List<Team> getAll() {
+        return teamService.getAll();
+    }
+    
+    @RequestMapping(value = "/team", method = RequestMethod.POST, produces = "application/json")
+    public Team create(@RequestBody Team team) {
+    	teamService.add(team);
+        return team;
+    }
+    
+    @RequestMapping(value = "/team", method = RequestMethod.PUT)
+    public void update(@RequestBody Team team) {
+    	teamService.update(team);
+    }
+    
+    @RequestMapping(value = "/team/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "id") int id) {
+    	teamService.delete(id);
+    }
+}
