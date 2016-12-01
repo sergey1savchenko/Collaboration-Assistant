9a3a441fa151f48830ce7b5b8b805f1868f73e89
xoxolische <nikitos_ua@ukr.net>
2016-12-01 11:56:53 +0200

    Add files via upload
    REST hrFeedback and Attach controllers

--- src/main/java/com/netcracker/ca/controller/api/AttachmentController.java
+++ src/main/java/com/netcracker/ca/controller/api/AttachmentController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +18,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.netcracker.ca.model.Attachment;
 import com.netcracker.ca.service.AttachmentFactory;
 import com.netcracker.ca.service.AttachmentService;
+import com.netcracker.ca.service.ProjectService;
+import com.netcracker.ca.service.TeamService;
 import com.netcracker.ca.utils.StorageException;
+
 
 @RestController
 public class AttachmentController extends BaseApiController {
@@ -39,16 +47,17 @@ public class AttachmentController extends BaseApiController {
 
 	@GetMapping("admin/api/project/{projectId}/files")
 	public List<Attachment> getProjectAttachments(@PathVariable int projectId) {
+	    System.out.println(attService.getProjectAttachments(projectId));
 		return attService.getProjectAttachments(projectId);
 	}
 
 	@PostMapping("admin/api/project/{projectId}/file")
-	public Attachment uploadForProject(@RequestParam("file") MultipartFile file, @RequestParam("text") String text,
+	public Attachment uploadForProject(@RequestParam MultipartFile file, @RequestParam("text") String text,
 			@PathVariable int projectId) throws IOException {
 		//add validation for file (not empty, <maxSize)
 		try (InputStream input = file.getInputStream()) {
 			Attachment att = attFactory.build(file.getName(), text, file.getContentType(), projectId, false);
-			return attService.addToProject(att,input, projectId);
+			return attService.add(att,input);
 		}
 	}
 
@@ -71,5 +80,50 @@ public class AttachmentController extends BaseApiController {
 	public void handleIOException(IOException ex) {
 		logger.error("Failed to process file", ex);
 	}
-
+	
+	@GetMapping("curator/api/team/{teamId}/files")
+	public List<Attachment> getTeamAttachments(@PathVariable int teamId) {
+	    System.out.println(attService.getTeamAttachments(teamId));
+		return attService.getTeamAttachments(teamId);
+	}
+	
+	@PostMapping("curator/api/team/{teamId}/file")
+	public Attachment uploadForTeam(@RequestParam MultipartFile file,@RequestParam("project_id") int project_id, @RequestParam("text") String text,
+			@PathVariable int teamId) throws IOException {
+		//add validation for file (not empty, <maxSize)
+		try (InputStream input = file.getInputStream()) {
+			Attachment att = attFactory.build(file.getName(), text, file.getContentType(), project_id, true);
+			return attService.add(att,input);
+		}
+	}
+	
+	@GetMapping("curator/api/file/{fileId}")
+	public ResponseEntity<Resource> curDownload(@PathVariable int fileId) throws IOException {
+		Resource resource = attService.getAsResource(fileId);
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("Content-Disposition", "attachment; filename=" + resource.getFilename().replace(" ", "_"));
+		responseHeaders.set("Content-Length", String.valueOf(resource.contentLength()));
+		return new ResponseEntity<Resource>(resource, responseHeaders, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("curator/api/file/{fileId}")
+	public void curDelete(@PathVariable int fileId) {
+		attService.delete(fileId);
+	}
+	
+	@GetMapping("student/api/team/{teamId}/files")
+	public List<Attachment> getStudentTeamAttachments(@PathVariable int teamId) {
+	    System.out.println(attService.getTeamAttachments(teamId));
+		return attService.getTeamAttachments(teamId);
+	}
+	
+	@GetMapping("student/api/file/{fileId}")
+	public ResponseEntity<Resource> stdDownload(@PathVariable int fileId) throws IOException {
+		Resource resource = attService.getAsResource(fileId);
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("Content-Disposition", "attachment; filename=" + resource.getFilename().replace(" ", "_"));
+		responseHeaders.set("Content-Length", String.valueOf(resource.contentLength()));
+		return new ResponseEntity<Resource>(resource, responseHeaders, HttpStatus.OK);
+	}
+	
 }
---
+++ src/main/java/com/netcracker/ca/controller/api/hrFeedbackController.java
@@ -0,0 +1,56 @@
+package com.netcracker.ca.controller.api;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import com.netcracker.ca.model.Feedback;
+import com.netcracker.ca.service.FeedbackService;
+
+
+
+
+@RestController
+public class hrFeedbackController extends BaseApiController{
+    
+    Logger logger = LogManager.getLogger("Error.Files");
+
+    @Autowired
+    private FeedbackService feedbackService;
+       
+    
+    @RequestMapping(value = "/hr/feedback/hrFeedbackStudent{app_form_id}", method = RequestMethod.GET, produces = "application/json")
+    public List<Feedback> get(@PathVariable(value = "app_form_id") int app_form_id) {	
+	System.out.println(feedbackService.getById(app_form_id));
+	
+	//here must be method to getByAppFormId();
+	//or student`s id
+
+        return feedbackService.getAll();
+    }
+    
+    
+    
+    @RequestMapping(value = "/hr/hrFeedback/hrFeedbackStude", method = RequestMethod.POST, produces = "application/json")
+    public Feedback add(@RequestBody Feedback feedback) {
+	//feedbackService.add(new Feedback(0, feedback.getGeneralReport(), feedback.getTechReport(), feedback.getInterviewer(), feedback.getInterviewStatus(), , student, project));
+	System.out.println(feedback);
+        return feedback;
+    }
+    
+    @RequestMapping(value = "/hr/hrFeedback/hrFeedbackStudent", method = RequestMethod.PUT)
+    public void update(@RequestBody Feedback feedback) {
+        feedbackService.update(feedback);
+    }
+     
+
+    @RequestMapping(value = "/hrFeedback{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "id") int id) {
+	feedbackService.delete(id);
+    }
+    
+}
